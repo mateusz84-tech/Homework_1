b4c0f0662f926effe9cecd8d31ee038c4dec1047
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-03-29 16:35:48 +0200

    Tworzenie i pobieranie ziaren Spring

--- src/main/java/pl/coderslab/AppConfig.java
+++ src/main/java/pl/coderslab/AppConfig.java
@@ -1,8 +1,17 @@
 package pl.coderslab;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.coderslab.beans.SimpleCustomerLogger;
 
 @Configuration
 
 public class AppConfig {
+
+    // zadanie 2
+    @Bean
+    public SimpleCustomerLogger scl(){
+
+        return new SimpleCustomerLogger();
+    }
 }
--- src/main/java/pl/coderslab/SpringDiApplication.java
+++ src/main/java/pl/coderslab/SpringDiApplication.java
@@ -1,12 +1,17 @@
 package pl.coderslab;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pl.coderslab.beans.SimpleCustomerLogger;
 
 public class SpringDiApplication {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // zadanie 2
+        SimpleCustomerLogger slc = context.getBean(SimpleCustomerLogger.class);
+        slc.log();
 
     }
 }
