d46b4f0f59fe47650f50415c62c72bf82b7dccf0
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-03-29 16:34:01 +0200

    Zadaanie 2

---
+++ src/main/java/pl/coderslab/beans/SimpleCustomerLogger.java
@@ -0,0 +1,15 @@
+package pl.coderslab.beans;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+@Component
+public class SimpleCustomerLogger implements CustomerLogger {
+
+    @Override
+    public void log() {
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime + ": Customer operation");
+    }
+}
